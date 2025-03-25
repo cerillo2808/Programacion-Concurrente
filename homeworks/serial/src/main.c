093aafc6550c31a579977ab7f333b6b6a3a843b5
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2025-03-24 22:13:15 -0600

    lectura de archivo job

--- homeworks/serial/src/main.c
+++ homeworks/serial/src/main.c
@@ -14,8 +14,22 @@ int main(int argc, char *argv[]){
 
     } else{
         // son 2 argumentos
-        // TO-DO: verificar que el archivo abre
-        printf("Cargando información...\n");
+
+        char *jobPath = argv[1];
+
+        printf("Intentando abrir: '%s'\n", jobPath);
+        FILE *jobFile = fopen(jobPath, "r");
+        // el parámetro r es para lectura (read)
+
+        if (jobFile==NULL){
+            printf("No se pudo abrir. Hay un error con el nombre o path de su archivo.\n");
+            return 1;
+
+        } else{
+            printf("Archivo %s abierto correctamente. Cargando información...\n", jobPath);
+            // TO-DO: leer cada linea del job e ir inicializando el objeto plate.
+        }
+        
     }
     return 0;
 }
