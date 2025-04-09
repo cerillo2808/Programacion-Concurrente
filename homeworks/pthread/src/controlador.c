582f40d2ab7fc4d63f45d9953a5a2394a89c950f
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2025-04-09 00:24:31 -0600

    copia de serial a pthread

---
+++ homeworks/pthread/src/controlador.c
@@ -0,0 +1,75 @@
+// Copyright [2025] <Liqing Yosery Zheng Lu>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <plate.h>
+#include <inttypes.h>
+#include <controlador.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <math.h>
+#include <escritor_archivos.h>
+#include <simulacion.h>
+
+int verificar_argumentos(int argc, char *argv[]) {
+    if (argc > 3) {
+        // muchos argumentos
+        printf("Error: Hay más argumentos de los necesarios. Ingrese la "
+                "dirección del archivo y la cantidad de hilos a utilizar.\n");
+        return 1;
+
+    } else if (argc <= 1) {
+        // faltan argumentos
+        printf("Error: Hay menos argumentos de los necesarios. Ingrese la "
+                "dirección del archivo y la cantidad de hilos a utilizar.\n");;
+        return 1;
+
+    } else {
+        // hay al menos un argumento
+        // TO-DO: verificar que se haya ingresado cantidad hilos (tarea 2)
+
+        char *jobPath = argv[1];
+
+        FILE *jobFile = fopen(jobPath, "r");
+        // el parámetro r es para lectura (read)
+
+        if (jobFile == NULL) {
+            printf("Error: No se pudo abrir. Hay un error con el nombre o path "
+                    "de su archivo.\n");
+            return 1;
+        } else {
+            // guardar de forma job###
+            char nombreJob[256];
+            // buscar el / del job
+            char *base = strrchr(jobPath, '/');
+            if (base) {
+                // saltarse el /
+                base++;
+            } else {
+                base = jobPath;
+            }
+
+            strncpy(nombreJob, base, sizeof(nombreJob));
+
+            // eliminar la extensión .txt
+            char *punto = strrchr(nombreJob, '.');
+            if (punto && strcmp(punto, ".txt") == 0) {
+                // el punto se vuelve caracter nulo
+                *punto = '\0';
+            }
+
+            char linea[256];
+            // buffer para cada linea de job, máximo de 256 chars
+
+            while (fgets(linea, sizeof(linea), jobFile)) {
+                // crear un plate para cada linea del txt
+                if (crear_plate(linea, nombreJob)) {
+                    return 1;
+                }
+            }
+            fclose(jobFile); //NOLINT
+        }
+    }
+    return 0;
+}
---
+++ homeworks/pthread/src/plate.c
@@ -0,0 +1,79 @@
+// Copyright [2025] <Liqing Yosery Zheng Lu>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <plate.h>
+#include <inttypes.h>
+#include <controlador.h>
+
+int crear_plate(const char *linea, const char *nombreJob) {
+    Plate plate;
+
+    // lee las lineas del plate###.bin
+    sscanf(linea, "%s %lf %lf %lf %lf", plate.nombreArchivo, &plate.delta,
+             &plate.alfa, &plate.h, &plate.epsilon);
+    char rutaCompleta[512];
+    // la ruta completa sólo puede medir 512 como máximo
+    snprintf(rutaCompleta, sizeof(rutaCompleta), "tests/%s",
+             plate.nombreArchivo);
+    // agregarle a nombre de archivo el path completo
+
+    sscanf(nombreJob, "%s", plate.nombreJob);
+
+    // abrir el archivo bin
+    FILE *bin = fopen(rutaCompleta, "rb");
+
+    if (leer_plate(rutaCompleta, &plate, bin)) {
+        return 1;
+    }
+
+    // pedir memoria para matriz que se llama temperaturas
+    double *temperaturas = (double *)malloc(plate.R * plate.C * sizeof(double));
+    if (!temperaturas) {
+        printf("Error: No se pudo asignar memoria para la matriz.\n");
+        fclose(bin);
+        return 1;
+    }
+
+    // subir temperaturas a la matriz
+    fread(temperaturas, sizeof(double), plate.R * plate.C, bin);
+
+    // simular la dispersión del calor
+    cambio_temperatura(temperaturas, plate);
+
+    // liberar memoria después de la simulación
+    free(temperaturas);
+
+    return 0;
+}
+
+int leer_plate(const char *nombreBin, Plate *plate, FILE *bin) {
+    if (!bin) {
+        printf("Error: No se pudo abrir el archivo binario '%s'.\n", nombreBin);
+        return 1;
+    }
+
+    // R por rows, filas en inglés
+    uint64_t R;
+    // C por columnas
+    uint64_t C;
+
+    // leer la cantidad de filas y columnas
+    fread(&R, sizeof(uint64_t), 1, bin);
+    fread(&C, sizeof(uint64_t), 1, bin);
+
+    // verificar que R y C sean válidos
+    if (R == 0 || C == 0) {
+        printf("Error: Dimensiones inválidas en el archivo binario '%s'.\n",
+                 nombreBin);
+        fclose(bin); //NOLINT
+        return 1;
+    }
+
+    // asignar valores a los atributos
+    plate->R = R;
+    plate->C = C;
+
+    return 0;
+}
